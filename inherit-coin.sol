08ab9eb25146fa3ef55d9fc994b62233d9a2d28e
MapleLaker <31602441+MapleLaker@users.noreply.github.com>
2017-09-18 22:15:49 +0800

    Create inherit-coin.sol

---
+++ inherit-coin.sol
@@ -0,0 +1,44 @@
+
+contract owned {
+    address public owner;
+
+    function owned() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
+}
+
+contract Coin is owned{
+//关键字“public”使变量能从合约外部访问。
+    address public minter;
+    mapping (address => uint) public balances;
+
+//事件让轻客户端能高效的对变化做出反应。
+    event Sent(address from, address to, uint amount);
+
+//这个构造函数的代码仅仅只在合约创建的时候被运行。
+    function Coin() {
+        owner = msg.sender;
+    }
+    
+    function mint(address receiver, uint amount) onlyOwner{
+        //if (msg.sender != owner) return;
+        balances[receiver] += amount;
+    }
+    
+    function send(address receiver, uint amount) {
+        if (balances[msg.sender] < amount) return;
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        
+        Sent(msg.sender, receiver, amount);
+    }
+}
