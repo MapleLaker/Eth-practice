032a69b191abe1f16acc9406ce56968ae60dbbaf
MapleLaker <31602441+MapleLaker@users.noreply.github.com>
2017-09-18 21:55:55 +0800

    Update coin.sol

--- coin.sol
+++ coin.sol
@@ -19,6 +19,7 @@ contract Coin {
         if (balances[msg.sender] < amount) return;
         balances[msg.sender] -= amount;
         balances[receiver] += amount;
+        
         Sent(msg.sender, receiver, amount);
     }
 }
